f956b01eab59afeeb0f7b750d6ce77d07ade8163
BrowserAdventures <garretbonkowski@protonmail.com>
2018-05-07 10:01:11 -0700

    added redux-devtools-extension

--- src/index.js
+++ src/index.js
@@ -7,16 +7,20 @@ import { BrowserRouter } from 'react-router-dom';
 
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducer from './rootReducer'
 
 
-const store = createStore(rootReducer, applyMiddleware(thunk))
+const store = createStore(rootReducer, composeWithDevTools(
+    applyMiddleware(thunk)
+))
 
 const app = (<BrowserRouter>
     <Provider store={store}>
         <App />
     </Provider>
 </BrowserRouter>)
+
 
 ReactDOM.render( app, document.getElementById( 'root' ) );
